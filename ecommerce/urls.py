419eb271731c66766dc8986aafc924d7c3174319
Doga Yilmaz <yilmaz.doga@gmail.com>
2021-03-21 15:48:02 +0300

    Created a basic site with html templates.

---
+++ ecommerce/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='ecommerce-home'),
+    path('about/', views.about, name='ecommerce-about'),
+]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+items = [
+    {
+        'seller': 'Doga Yilmaz',
+        'product_name': 'Rocket Appartamento Manual Espresso Machine',
+        'description': 'Best espresso machine ever.'
+    },
+    {
+        'seller': 'Sarp Sunar',
+        'product_name': 'Rocket Coffee Grinder',
+        'description': 'Sample description.'
+    }
+]
+
+
+def home(request):
+    context = {
+        'title': 'Home',
+        'items': items
+    }
+    return render(request, 'ecommerce/home.html', context)
+
+
+def about(request):
+    return render(request, 'ecommerce/about.html')
