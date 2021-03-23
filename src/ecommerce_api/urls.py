ead04c4f2d941f9deedb3d90a7bba521395f7916
Doga Yilmaz <yilmaz.doga@gmail.com>
2021-03-23 21:57:31 +0300

    registration url bug fix

--- src/ecommerce_api/urls.py
+++ src/ecommerce_api/urls.py
@@ -6,6 +6,7 @@ from core.views import ItemView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', admin.site.urls),
     path('api/items', ItemView.as_view(), name='item'),
 ]
