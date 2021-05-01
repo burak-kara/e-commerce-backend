2b1c8fb419c479e539f8a6f3681123b21c48ee46
Doga Yilmaz <yilmaz.doga@gmail.com>
2021-05-01 19:16:14 +0300

    minor refactoring

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -143,7 +143,7 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']
 }
 
 SITE_ID = 1
