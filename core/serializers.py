f0234babb0fb824233f6f6888e33deaa091088a3
Doga Yilmaz <yilmaz.doga@gmail.com>
2021-05-28 11:08:28 +0300

    wallet adress bug fix

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,36 @@
 from rest_framework import serializers
 from .models import Item, User, Category, Order, Review, Campaign, Advertisement
+from django.conf import settings
+import uuid
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+import json
+
+private_key_master = settings.PRIVATE_KEY
+public_key_master = '0xB78DFDdF8af06485b5358ad98950119F6f270AE4'
+
+contract_address = '0x1781684a1A5eff097C631E227d654a3470842e45'
+
+
+def initialize_chain_connection():
+    w3 = Web3(Web3.HTTPProvider(
+        "https://data-seed-prebsc-2-s1.binance.org:8545/"))  # "1-s2 provider has the most uptime" - Emir
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)  # might cause errors lul
+    return w3
+
+
+def create_wallet():
+    w3 = initialize_chain_connection()
+    created_wallet_address = w3.eth.account.create()
+    return created_wallet_address.address, created_wallet_address.privateKey.hex()
+
+
+w3 = initialize_chain_connection()
+contract_abi_directory = './static/blockchain/contract_abi.json'
+f = open(contract_abi_directory)
+temp_abi = json.load(f)
+contract = w3.eth.contract(address=contract_address, abi=temp_abi)
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password_validation = serializers.CharField(
@@ -8,12 +39,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_number', 'first_name', 'last_name', 'is_sales_manager',
-                  'is_product_manager', 'password', 'password_validation', 'wallet_address','private_wallet_address']
+                  'is_product_manager', 'password', 'password_validation']
         extra_kwargs = {
             'password': {
                 'write_only': True
             }
         }
+
+    public_key, private_key = create_wallet()
+    wallet_address = public_key
+    private_wallet_address = private_key
 
     def save(self, request):
         user = User(
@@ -24,8 +59,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             last_name=self.validated_data['last_name'],
             is_sales_manager=self.validated_data['is_sales_manager'],
             is_product_manager=self.validated_data['is_product_manager'],
-            wallet_address=self.validated_data['wallet_address'],
-            private_wallet_address=self.validated_data['private_wallet_address']
+            wallet_address=self.wallet_address,
+            private_wallet_address=self.private_wallet_address
         )
 
         password = self.validated_data['password']
@@ -38,49 +73,51 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 
-                  'phone_number', 
-                  'first_name', 
-                  'last_name', 
-                  'addresses', 
+        fields = ['username', 'email',
+                  'phone_number',
+                  'first_name',
+                  'last_name',
+                  'addresses',
                   'is_sales_manager',
-                  'is_product_manager', 
+                  'is_product_manager',
                   'is_admin',
                   'twoFA_enabled',
                   'balance',
                   'wallet_address',
                   ]
 
+
 class UserSelectSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['pk',
-                  'username',  
+                  'username',
                   'is_sales_manager',
                   'is_product_manager',
                   ]
-                  
+
+
 class UserPrivilegeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ['pk',
-              'username', 
-              'is_sales_manager',
-              'is_product_manager',
-              ]
-              
+    class Meta:
+        model = User
+        fields = ['pk',
+                  'username',
+                  'is_sales_manager',
+                  'is_product_manager',
+                  ]
 
 
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username',  
-                  'first_name', 
+        fields = ['username',
+                  'first_name',
                   'last_name',
-                  'balance', 
+                  'balance',
                   'wallet_address',
                   ]
 
